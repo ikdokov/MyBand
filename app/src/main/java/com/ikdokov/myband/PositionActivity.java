020d59bb3c590f08699608ed3e611ec4262b2ffa
Ivan Dokov <idokov@broadsoft.com>
2015-07-17 14:29:28 +0300

    Initial commmit

--- app/src/main/java/com/ikdokov/myband/PositionActivity.java
+++ app/src/main/java/com/ikdokov/myband/PositionActivity.java
@@ -1,25 +1,23 @@
 package com.ikdokov.myband;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class PositionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_position);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_position, menu);
         return true;
     }
 
